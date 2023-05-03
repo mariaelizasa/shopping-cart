d63b832ecbbea755af8866471968a5bce7eb88b0
Maria Eliza <eliza.sads@gmail.com>
2023-05-03 17:24:46 -0300

    feat: creating cart functions in context

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, useState } from "react";
 import { Product } from "../@types/Product";
 
 export type CartContextProps = {
   cart: Product[];
-  addItemCart: (product: Product) => void;
+  addItemCart: (product: Product, id: string) => void;
   removeItemCart: (id: string) => void;
-  itemAmount: number;
+  clearCart: () => void;
 };
 
 export const CartContext = React.createContext<CartContextProps>(
@@ -19,19 +19,49 @@ type CartContextProviderProps = {
 
 const CartContextProvider = ({ children }: CartContextProviderProps) => {
   const [cart, setCart] = React.useState<Product[]>([]);
-  const [itemAmount, setItemAmount] = useState(0);
 
   const addItemCart = (product: Product) => {
-    console.log("add item cart");
+    const { id } = product;
+    const newItem = { ...product, amount: 1 };
+
+    //check if alredy exist
+    const existCartItem = cart.find((item) => {
+      return item.id === id;
+    });
+
+    // if alredy exist, run the cart array and check if is the same id =>
+    // if yes, return the same item and add +1 to amount
+    // if not, return the item
+    if (existCartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: item.amount + 1 };
+        } else {
+          return item;
+        }
+      });
+      // create a new cart
+      setCart(newCart);
+    } else {
+      // add the new item to list
+      setCart([...cart, newItem]);
+    }
+  };
+
+  const clearCart = () => {
+    setCart([]);
   };
 
   const removeItemCart = (id: string) => {
-    console.log("add item cart");
+    const filteredCart = cart.filter((item) => {
+      return item.id !== id;
+    });
+    setCart(filteredCart);
   };
 
   return (
     <CartContext.Provider
-      value={{ cart, itemAmount, addItemCart, removeItemCart }}
+      value={{ cart, addItemCart, removeItemCart, clearCart }}
     >
       {children}
     </CartContext.Provider>
